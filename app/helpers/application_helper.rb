0d4e2dc6128fb9d9ece9a1cad8d072d3563dab44
FlorianHasuik <fhasuik@gmail.com>
2014-11-12 12:02:05 +0100

    Implementation Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  def title
+    base_titre = "Simple App du Tutoriel Ruby on Rails"
+    if @title.nil?
+      base_titre
+    else
+      "#{base_titre} | #{@title}"
+    end
+  end
+  
 end
